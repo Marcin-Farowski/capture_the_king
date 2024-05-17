3d58352febbe8cbfacbc593e8ec97f91478534e5
Marcin-Farowski <m.farowski@gmail.com>
2024-05-17 22:53:17 +0200

    Cannonical Tag, Internal Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,25 +9,35 @@ const catamaran = Catamaran({
 
 export const metadata: Metadata = {
   title: "Zdominuj Schwytaj Króla | Twoje Narzędzie do Sukcesu w Grze",
-  description: "Zdobądź przewagę w Schwytaj Króla dzięki naszej aplikacji: optymalizuj strategie, śledź postępy i korzystaj z zaawansowanych funkcji.",
-  keywords: ['Schwytaj Króla', 'Aplikacja do Schwytaj Króla', 'Narzędzie do Schwytaj Króla', 'Pomoc w Schwytaj Króla'],
-  creator: 'Marcin Farowski',
-  publisher: 'Marcin Farowski',
-  metadataBase: new URL('https://capture-the-king.vercel.app'),
+  description:
+    "Zdobądź przewagę w Schwytaj Króla dzięki naszej aplikacji: optymalizuj strategie, śledź postępy i korzystaj z zaawansowanych funkcji.",
+  keywords: [
+    "Schwytaj Króla",
+    "Aplikacja do Schwytaj Króla",
+    "Narzędzie do Schwytaj Króla",
+    "Pomoc w Schwytaj Króla",
+  ],
+  creator: "Marcin Farowski",
+  publisher: "Marcin Farowski",
+  metadataBase: new URL("https://capture-the-king.vercel.app"),
+  alternates: {
+    canonical: "/",
+  },
   openGraph: {
-    title: 'Zdominuj Schwytaj Króla',
-    description: 'Skorzystaj z naszej aplikacji, aby zdobywać przewagę w Schwytaj Króla.',
-    url: 'https://capture-the-king.vercel.app',
-    siteName: 'Zdominuj Schwytaj Króla',
+    title: "Zdominuj Schwytaj Króla",
+    description:
+      "Skorzystaj z naszej aplikacji, aby zdobywać przewagę w Schwytaj Króla.",
+    url: "https://capture-the-king.vercel.app",
+    siteName: "Zdominuj Schwytaj Króla",
     images: [
       {
-        url: 'https://capture-the-king.vercel.app/assets/schwytaj-krola.jpg',
+        url: "https://capture-the-king.vercel.app/assets/schwytaj-krola.jpg",
         width: 616,
         height: 353,
       },
     ],
-    locale: 'pl_PL',
-    type: 'website',
+    locale: "pl_PL",
+    type: "website",
   },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,19 @@ export default function Home() {
     <main className="bg-zinc-950">
       <div className="flex mx-auto flex-col max-w-screen-xl min-h-screen items-center bg-zinc-950 px-4 py-10">
         <h1 className="pb-3 text-4xl font-semibold text-zinc-50">
-          Schwytaj Króla
+          <Link href="https://capture-the-king.vercel.app">Schwytaj Króla</Link>
         </h1>
-        <h2 className="pb-12 text-md font-light text-zinc-50">Zdominuj Schwytaj Króla dzięki naszej aplikacji!</h2>
+        <h2 className="pb-12 text-md font-light text-zinc-50">
+          Zdominuj Schwytaj Króla dzięki naszej aplikacji!
+        </h2>
         <Board />
         <div className="flex w-full justify-between text-xs font-thin text-zinc-200 mt-auto">
-          <Link href="https://pl-wiki.metin2.gameforge.com/index.php/Schwytaj_Kr%C3%B3la">Schwytaj Króla Wiki</Link>
-          <Link href="https://github.com/Marcin-Farowski/capture_the_king">GitHub</Link>
+          <Link href="https://pl-wiki.metin2.gameforge.com/index.php/Schwytaj_Kr%C3%B3la">
+            Schwytaj Króla Wiki
+          </Link>
+          <Link href="https://github.com/Marcin-Farowski/capture_the_king">
+            GitHub
+          </Link>
         </div>
       </div>
     </main>
